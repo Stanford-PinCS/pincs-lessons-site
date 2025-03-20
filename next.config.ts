45de58c94e2b458c518bb3da08669ab62e053e90
Charlie Meyer <charliemeyer@fastmail.com>
2025-03-20 07:55:48 -0400

    rebuild with base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/pickcode",
+  assetPrefix: "/pickcode",
 };
 
 export default nextConfig;
