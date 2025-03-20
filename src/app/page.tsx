fb4d74e58cb7b34725b20670ea1a35ea2de93eb9
Charlie Meyer <charliemeyer@fastmail.com>
2025-03-20 08:05:34 -0400

    include example lesson

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="h-[100vh] w-full flex items-center justify-center">
+    <div className="h-[100vh] w-full flex flex-col items-center justify-center gap-2">
       <h1>PinCS lesson site coming soon!</h1>
+      <Link href={"/example-lesson"} className="underline">
+        Go to example lesson
+      </Link>
     </div>
   );
 }
