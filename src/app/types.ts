eb2f839f96f56b1fb533399e4a6dcc3727217aa3
Trip Master <trip@kira-learning.com>
2025-05-27 17:19:22 -0400

    updates to front page and types

---
+++ src/app/types.ts
@@ -0,0 +1,25 @@
+export enum Subject {
+    Biology = "Biology",
+    Chemistry = "Chemistry",
+    Physics = "Physics",
+    English = "English",
+    Music = "Music",
+    Mathematics = "Mathematics",
+    ComputerScience = "Computer Science",
+    History = "History"
+}
+
+
+export type Tag =
+  | { type: "subject"; value: Subject }
+  | { type: "grade"; value: number | undefined }
+
+export function tagsMatch (tagA: Tag, tagB: Tag) {
+  return tagA.type === tagB.type && tagA.value === tagB.value
+}
+
+export type Lesson = {
+    title: string;
+    link: string;
+    tags: Tag[];
+}
