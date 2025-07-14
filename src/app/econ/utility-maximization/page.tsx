c651d392ccb390905d8ff6d08a69187c52896d04
Preston Seay <preston.seay@outlook.com>
2025-07-14 13:34:03 -0700

    added link for utility lesson teacher guide

--- src/app/econ/utility-maximization/page.tsx
+++ src/app/econ/utility-maximization/page.tsx
@@ -4,7 +4,7 @@ import UtilityLesson from "./lesson/UtilityMaximizationLesson";
 export default function ExampleLessonLandingPage() {
   return (
     <LandingPage
-      teacherResources=""
+      teacherResources="https://docs.google.com/document/d/1p1tbT99rhCyXwKmXK0qy7QEuGwj2zdnClWqCE6ld0_o/edit?usp=sharing"
       lessonDescription="This lesson targets topics 1.5 and 1.6 of AP Microeconomics, although it can be suitable for non-AP level economics students too. Specifically, it teaches students about Marginal Utility, Marginal Cost, and Marginal Benefit. It also builds intuition for the Law of Diminishing Maringal Utility and teaches students about how to optimize for utility using the MU/P ratio. In the end, student's will code up the logic for a greedy algorithm, solidifying their understanding of all of these topics."
     >
       {" "}
