5f2383595f654c34b045111a12470be561e07355
Hoyoon Song <hoyoonsong06@gmail.com>
2025-07-17 10:55:03 -0700

    UI improvements in collapsible component, in addition to minor changes

--- src/components/Collapsible.tsx
+++ src/components/Collapsible.tsx
@@ -15,16 +15,25 @@ const Collapsible = ({
   const toggleSymbol = isOpen ? "🔼" : "🔽";
   return (
     <div>
-      <button className="cursor-pointer text-left" onClick={toggle}>
+      <button
+        className="cursor-pointer text-left"
+        style={{
+          border: "1px solid #e0e0e0",
+          padding: "10px",
+          width: "100%",
+          textAlign: "left",
+        }}
+        onClick={toggle}
+      >
         {children} {toggleSymbol}
-      </button>
-      {isOpen && (
-        <div>
-          <div style={{ paddingLeft: "1rem", marginBottom: "1rem" }}>
-            {ExampleContent}
+        {isOpen && (
+          <div>
+            <div style={{ paddingLeft: "1rem", marginBottom: "1rem" }}>
+              {ExampleContent}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </button>
     </div>
   );
 };
