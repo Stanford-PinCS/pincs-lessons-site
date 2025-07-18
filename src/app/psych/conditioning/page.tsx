2bf2ff61ad37e476260b8c75ec63d7ea357e7cd5
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2025-07-18 14:37:22 -0700

    remove email

--- src/app/psych/conditioning/page.tsx
+++ src/app/psych/conditioning/page.tsx
@@ -5,7 +5,7 @@ export default function ExampleLessonLandingPage() {
   return (
     <LandingPage
       teacherResources="https://docs.google.com/document/d/1J8WhZ0jWdM5s1A5n-_vIYYcWcSrPBmWh71ZlL45Wk9U/edit?usp=sharing"
-      lessonDescription="Conditioning lesson in progress. Feedback welcome! (shrish p [at] stanford [dot] edu)"
+      lessonDescription="Conditioning lesson in progress. Feedback welcome!"
     >
       {" "}
       <ConditioningLesson />{" "}
