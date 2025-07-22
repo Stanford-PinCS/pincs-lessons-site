6fe5f4727b9360d64b9837c9e5c847a1d9bf0b80
Preston Seay <preston.seay@outlook.com>
2025-07-22 10:27:07 -0700

    added link to teacher resources

--- src/app/physics/forces/page.tsx
+++ src/app/physics/forces/page.tsx
@@ -4,7 +4,7 @@ import ForcesLesson from "./lesson/ForcesLesson";
 export default function ExampleLessonLandingPage() {
   return (
     <LandingPage
-      teacherResources=""
+      teacherResources="https://docs.google.com/document/d/15WGKIbQEHPWGp4cyRWQAMTp8okSxzOi6hhinJ8qXPzE/edit?usp=sharing"
       lessonDescription="This lesson teaches the basics of forces, useful for any high school physics class. It will walk students through what a force is, how to deal with vectors (breaking down a force vector into its components), how to make a free body diagram, and how to add forces. Along the way, students will have put the steps of adding forces into logic, strengthening the steps of component-wise vector addition."
     >
       <ForcesLesson />
