7790038b04a4d15ccfb577b5357f51ee25fff1c9
Preston Seay <preston.seay@outlook.com>
2025-07-31 10:15:00 -0700

    moved puck config to seperate file

---
+++ src/app/lesson-maker/puck.config.tsx
@@ -0,0 +1,102 @@
+"use client";
+import Block from "@/components/Block";
+import Lesson from "@/components/Lesson";
+import LessonWrapper from "@/components/LessonWrapper";
+import { Config } from "@measured/puck";
+import { BlockProps } from "../../components/Block";
+
+const BlockColor = {
+  type: "radio" as const,
+  options: [
+    {
+      label: "Green (use for lesson overview and recap)",
+      value: "green",
+    },
+    {
+      label: "Blue (use for background information and definitions)",
+      value: "blue",
+    },
+    { label: "Yellow (use for interactive portions)", value: "yellow" },
+    { label: "Purple (use for quizzes)", value: "purple" },
+  ],
+};
+const Text = { type: "text" as const };
+const TextArea = { type: "textarea" as const };
+const Slot = { type: "slot" as const };
+
+export const config: Config = {
+  components: {
+    Paragraph: {
+      fields: {
+        text: TextArea,
+      },
+      defaultProps: {
+        text: "This is a paragraph.",
+      },
+      render: ({ text }) => {
+        return <p>{text}</p>;
+      },
+    },
+    Block: {
+      fields: {
+        title: TextArea,
+        color: BlockColor,
+        children: Slot,
+      },
+      defaultProps: {
+        color: "green",
+        title: "Title",
+      },
+      render: ({ children: Children, color, title }) => {
+        return (
+          <Block color={color} title={title}>
+            <Children />
+          </Block>
+        );
+      },
+    },
+    Lesson: {
+      fields: {
+        slideIndex: {
+          type: "number",
+        },
+        slides: {
+          type: "array",
+          arrayFields: {
+            title: TextArea,
+            color: BlockColor,
+            children: Slot,
+          },
+        },
+      },
+      defaultProps: {
+        blocks: [],
+        slideIndex: 0,
+      },
+      render: ({ blocks, slideIndex }) => {
+        if (!blocks || typeof blocks !== "object") {
+          return <>Error: Lesson component must have slides.</>;
+        }
+        const blockArray = [
+          { color: "green" as const, title: "Title", children: undefined },
+          { color: "blue" as const, title: "Title2", children: undefined },
+        ];
+        const slides = blockArray.map(
+          ({ children, color, title }: BlockProps) => {
+            children = children || <p>This is a paragraph</p>;
+            return (
+              <Block children={children} color={color} title={title}></Block>
+            );
+          }
+        );
+
+        return <Lesson slideIndex={slideIndex - 1} slides={slides} />;
+      },
+    },
+  },
+  root: {
+    render: ({ children }) => {
+      return <LessonWrapper>{children}</LessonWrapper>;
+    },
+  },
+};
