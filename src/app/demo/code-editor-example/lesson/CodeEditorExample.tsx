c5b6af9176f02ed26226185613c2901b2e7156d7
Charlie Meyer <charliemeyer@fastmail.com>
2025-08-07 08:44:25 -0400

    running code works

--- src/app/demo/code-editor-example/lesson/CodeEditorExample.tsx
+++ src/app/demo/code-editor-example/lesson/CodeEditorExample.tsx
@@ -21,7 +21,7 @@ export const CodeEditorExample: React.FC = () => {
       <CodeEditor
         instructionsMarkdown={instructionsMarkdown}
         lessonId="test-code-editor"
-        pluginId="force-addition"
+        pluginId="chat-demo"
         height={600}
         language={"python"}
       />
